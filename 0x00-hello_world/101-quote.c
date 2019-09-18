51efa7a4c2f7618fbbe492dba805997a6062db33
GiovanniAyala <op7puro@live.com>
2019-09-18 15:04:26 -0500

    Update task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
