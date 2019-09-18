018ebe190dd035a8837f2239d30e723d28ed27d1
GiovanniAyala <op7puro@live.com>
2019-09-18 13:51:40 -0500

    Update task 05

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,%c", '\n');
 
 return (0);
 }
