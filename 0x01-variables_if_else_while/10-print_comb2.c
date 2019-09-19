8effa0034235f759d4c6b073f6cb5d924ee174c0
GiovanniAyala <op7puro@live.com>
2019-09-19 16:57:39 -0500

    Update task 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -4,21 +4,21 @@
  *
  * Return: Always 0 (Success)
  */
- int main(void)
- {
- int i, c, j;
- for (i = 0; i < 100; i++)
- {
- c = i / 10;
- j = i % 10;
- putchar(c + '0');
- putchar(j + '0');
- if (i != 99)
- {
- putchar(',');
- putchar(' ');
- }
- }
- putchar('\n');
- return (0);
- }
+int main(void)
+{
+int i, c, j;
+for (i = 0; i < 100; i++)
+{
+c = i / 10;
+j = i % 10;
+putchar(c + '0');
+putchar(j + '0');
+if (i != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
