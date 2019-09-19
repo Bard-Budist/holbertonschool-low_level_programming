7fcce918b4974f442f360bda035512c129e6d81e
GiovanniAyala <op7puro@live.com>
2019-09-19 12:09:51 -0500

    Add more task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  for (int i='a';i <='z'; i++){
+    putchar(i);
+  }
+  putchar('\n');
+}
