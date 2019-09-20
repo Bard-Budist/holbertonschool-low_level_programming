408876964e42908864098147e1dcdefd321e75e6
GiovanniAyala <op7puro@live.com>
2019-09-20 17:59:28 -0500

    Fix task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,19 +5,19 @@
  * Return: Always 0 (Success)
  */
 int main(void) {
-    int i, n;
-    for (n = 1; n < 90; n++) {
-        while(n < 90) {
-            if (i / 10 % 10 > i % 10)
-            break;
-            if ((i /= 10) == 0) {
-              putchar(n + '0');
-              if (n != 89) {
-                putchar(',');
-                putchar(' ');
-              }
-              break;
-            }
-        }
-    }
+int i, n;
+for (n = 1; n < 90; n++) {
+while(n < 90) {
+if (i / 10 % 10 > i % 10)
+break;
+if ((i /= 10) == 0) {
+putchar(n + '0');
+if (n != 89) {
+putchar(',');
+putchar(' ');
+}
+break;
+}
+}
+}
 }
