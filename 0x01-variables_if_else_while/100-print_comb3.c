0135d44ace4a52e7d75768e69d2aa6ffae21c075
GiovanniAyala <op7puro@live.com>
2019-09-20 17:47:00 -0500

    Add task Advance

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) {
+    int i, n;
+    for (n = 1; n < 90; n++) {
+        while(n < 90) {
+            if (i / 10 % 10 > i % 10)
+            break;
+            if ((i /= 10) == 0) {
+              putchar(n + '0');
+              if (n != 89) {
+                putchar(',');
+                putchar(' ');
+              }
+              break;
+            }
+        }
+    }
+}
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,17 +1,35 @@
-#include <stdlib.h>
-
-int main()
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  int i, j;
-  for(i=00;i<=99;i++)
-  {
-    for(j=00;j<=99;j++)
-      {
-	putchar(i);
-	putchar(' ');
-	putchar(j);
-	putchar(',');
-	putchar('\n');
-      }
-  }
+int i, k, c, j,f,p;
+for (i = 0; i < 100; i++)
+{
+for (f= 0; f< 100; f++) {
+    /* code */
+
+c = i / 10;
+j = i % 10;
+k = f / 10;
+p = f % 10;
+if ((i / 10 % 10 > i % 10) || (f / 10 % 10 > f % 10))
+break;
+
+putchar(c + '0');
+putchar(j + '0');
+putchar(' ');
+putchar(k + '0');
+putchar(p + '0');
+
+putchar(',');
+putchar(' ');
+
+}
+}
+putchar('\n');
+return (0);
 }
