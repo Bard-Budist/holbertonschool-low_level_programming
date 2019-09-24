8fba029bd0e20795f9aeaa688ae5f99e7c168aaa
GiovanniAyala <op7puro@live.com>
2019-09-24 15:12:21 +0000

    Fix task 02

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,18 @@
 #include "holberton.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * print_alphabet_x10 - writes the character c to stdout
  *
- * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
 */
 void print_alphabet_x10(void)
 {
-	char c, n;
-	for (n = '0'; n <= '9'; n++)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
-	}
+char c, n;
+for (n = '0'; n <= '9'; n++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
 }
