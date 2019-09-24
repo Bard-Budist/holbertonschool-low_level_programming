edc3795b0d03bba867e246f00956439fc7308d62
GiovanniAyala <op7puro@live.com>
2019-09-24 15:32:45 +0000

    Fix task 06

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
 /**
- * _putchar - writes the character c to stdout
+ * _abs - Num abstract
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
 */
 int _abs(int c)
 {
-	return ((c > 0) ? c : c * -1);
+return ((c > 0) ? c : c * -1);
 }
