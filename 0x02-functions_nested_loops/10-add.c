bc9a06265c71a209a3e794b4c7dc96c828d9aa3a
GiovanniAyala <op7puro@live.com>
2019-09-24 19:31:40 +0000

    Fix task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int add(int a, int b)
+{
+	int suma = a + b;
+	return (suma);
+}
+
