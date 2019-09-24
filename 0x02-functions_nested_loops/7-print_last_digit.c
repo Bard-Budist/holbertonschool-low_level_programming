1374dda6702ba54104c412fc2ae6f311aa756a61
GiovanniAyala <op7puro@live.com>
2019-09-24 15:48:37 +0000

    Fix task 07

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,13 @@
 #include "holberton.h"
-
+/**
+* print_last_digit - writes the character c to stdout
+* @c: int parameter
+*
+* Return: Value of c
+*/
 int print_last_digit(int c)
 {
-	int i = (c > 0)?c % 10 : (c * -1) % 10;
-	_putchar(i + '0');
-	return i;
+int i = (c > 0) ? c % 10 : (c * -1) % 10;
+_putchar(i + '0');
+return (i);
 }
