1f2d805b1ff17e9d34d3fe76e5ad7f8dc6d3c04a
GiovanniAyala <op7puro@live.com>
2019-09-24 14:46:23 +0000

    Fix task 00

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,13 +4,18 @@
   *
   * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	char c[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-	int i;
-	for(i = 0; i < 9 ; i++ )
-		_putchar(c[i]);
-	_putchar ('\n');|:
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar ('\n');
 	return (0);
 }
+
