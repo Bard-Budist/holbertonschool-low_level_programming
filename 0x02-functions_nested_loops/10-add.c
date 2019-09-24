49871799aeccbc2524b720d4c094c2fa36671e6f
GiovanniAyala <op7puro@live.com>
2019-09-24 19:36:07 +0000

    Update task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
+ * add - writes the character c to stdout
+ * @a: The character to print
+ * @b: Second parameter
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
 */
