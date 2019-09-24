5742f3dcb6cbc029a59e074163febc71491d0370
GiovanniAyala <op7puro@live.com>
2019-09-24 20:56:43 +0000

    Fix task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (n = n; n <= 98; n++)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+}
+}
+else
+{
+for (n = n; n >= 98; n--)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf("\n");
+}
+}
+}
+printf("\n");
+}
