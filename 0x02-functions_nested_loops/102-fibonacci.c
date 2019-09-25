c0d46234037bc5b0021179a75c0ac280a26ee268
GiovanniAyala <op7puro@live.com>
2019-09-25 23:58:12 +0000

    Fix task 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,11 +9,11 @@ int main(void)
 long num1 = 0;
 long num2 = 1;
 long aux, i;
-for (i = 0; i <= 53; i++)
+for (i = 0; i < 53; i++)
 {
 aux = num1 + num2;
 printf("%ld", aux);
-if (i != 53)
+if (i != 52)
 putchar(',');
 num1 = num2;
 num2 = aux;
