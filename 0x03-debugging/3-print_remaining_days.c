16ac09512df682adc009fa9fa22ea0b54f055850
GiovanniAyala <op7puro@live.com>
2019-09-25 14:05:46 +0000

    Fix task 03

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,7 +24,7 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if (month >= 3 && day == 60)
+if (month == 3 && day == 60)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
