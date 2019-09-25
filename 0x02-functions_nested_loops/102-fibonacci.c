267aa4bedc6dcba8b5a19e088529d186fcf2a2ef
GiovanniAyala <op7puro@live.com>
2019-09-25 23:34:03 +0000

    Add task 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+int num1 = 0;
+int num2 = 1;
+int aux, i;
+for (i = 0; i < 50; i++)
+{
+aux = num1 + num2;
+printf("%d, ", aux);
+num1 = num2;
+num2 = aux;
+}
+}
