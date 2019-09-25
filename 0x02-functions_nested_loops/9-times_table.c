d274ff17e0767553497a1e5a14430db0f1126019
GiovanniAyala <op7puro@live.com>
2019-09-25 22:00:57 +0000

    Fix task 09

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,29 +7,28 @@
 void times_table(void)
 {
 int a, b, c;
-for (a = 0; a <= 9; a++)
-{
-for (b = 0; b <= 9; b++)
-{
-c = a * b;
-if (c < 10)
-{
-if (b > 0)
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-}
-_putchar(c + '0');
-}
-else
-{
-_putchar(',');
-_putchar(' ');
-_putchar(c / 10 + '0');
-_putchar(c % 10 + '0');
-}
-}
-_putchar('\n');
-}
-
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (c < 10)
+			{
+				if (b > 0)
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+	
+			}
+		}
+		_putchar('\n');
+	}
 }
