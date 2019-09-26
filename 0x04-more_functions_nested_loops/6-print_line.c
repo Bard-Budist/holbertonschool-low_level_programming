cf122eec7cb927e54e11b1bcbeda408e41da661c
GiovanniAyala <op7puro@live.com>
2019-09-26 23:50:23 +0000

    Add task 06

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_line - print _
+ * @n: Value of print _
+*/
+void print_line(int n)
+{
+if (n > 0)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
