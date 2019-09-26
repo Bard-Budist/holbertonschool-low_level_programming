36e30a3907c2f217964a1efd0cc4a3637928cfc4
GiovanniAyala <op7puro@live.com>
2019-09-26 23:32:42 +0000

    Add task 05

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * more_numbers - print numbers x10 at 0 to 14
+*/
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 15; j++)
+{
+if (j <= 9)
+_putchar(j + '0');
+
+else
+{
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
