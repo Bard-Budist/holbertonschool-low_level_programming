a42ff5ab220b29327fba2882eee6124cccd4ebe8
GiovanniAyala <op7puro@live.com>
2019-09-26 23:39:07 +0000

    Fix task 05

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,9 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 15; j++)
 {
-if (j <= 9)
-_putchar(j + '0');
-
-else
-{
-_putchar((j / 10) + '0');
+_putchar(((j <= 9) ? j : (j / 10)) + '0');
+if (j > 9)
 _putchar((j % 10) + '0');
-}
 }
 _putchar('\n');
 }
