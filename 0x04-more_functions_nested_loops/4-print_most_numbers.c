da115a4bf8866b48b38404dcfd4c34789a4e874a
GiovanniAyala <op7puro@live.com>
2019-09-26 23:22:16 +0000

    Add task 04

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+   * print_numbers - prints number 0 to 9
+   */
+void print_most_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+if (c != '2' && c != '4')
+_putchar(c);
+}
+_putchar('\n');
+}
