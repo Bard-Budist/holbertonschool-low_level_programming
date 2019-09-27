64d2a1df107a13d01eae38534d6230599047399f
GiovanniAyala <op7puro@live.com>
2019-09-27 00:14:35 +0000

    Add task 08

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_square - Print # n
+ * @n: Number of loops to
+*/
+void print_square(int n)
+{
+int i, j;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+_putchar('#');
+_putchar('\n');
+}
+}
+}
