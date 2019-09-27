79f61d9f2c3d1c11c3231f9c5dcf7b5f0d0c5e40
GiovanniAyala <op7puro@live.com>
2019-09-27 01:31:10 +0000

    Add task 09

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - print 0 to 100
+ *
+ * Return: Always 0 Succes
+*/
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz");
+else if (i % 5 == 0)
+printf("Buzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i < 10)
+printf("%d", i);
+else
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+}
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
