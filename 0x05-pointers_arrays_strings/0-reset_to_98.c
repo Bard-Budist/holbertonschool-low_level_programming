ffe95e7d4e08d03a92020581c4efbf773b24c400
GiovanniAyala <op7puro@live.com>
2019-09-30 20:44:14 +0000

    Add folder and project 0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - change value of pointer
+ * @n: Value of parameter
+ *
+**/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# Pointers :D# Pointers :D
