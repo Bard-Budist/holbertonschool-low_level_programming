d2e64f59ebee2707a49d893a5c46dfee28aaef32
GiovanniAyala <op7puro@live.com>
2019-09-30 20:51:39 +0000

    Add task 01

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * swap_int - change value of 2 parameter
+ * @a: first parameter
+ * @b: second parameter
+ *
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
