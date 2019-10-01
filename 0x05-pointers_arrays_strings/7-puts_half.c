50d85d9d34399cea5f6516278aa2e3361858a3da
GiovanniAyala <op7puro@live.com>
2019-10-01 20:03:02 +0000

    Add task 07

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * puts_half - Print mitad
+ * @str: Parameter of string
+ *
+*/
+void puts_half(char *str)
+{
+int len = 0, i = 0;
+char *str2 = str;
+while (str2[0] != '\0')
+{
+str2++;
+len++;
+i++;
+}
+for (i = ((len % 2 == 1) ? (len - 1) / 2 : len / 2) ; i <= len - 1; i++)
+{
+char t = str[i];
+_putchar(t);
+}
+_putchar('\n');
+}
