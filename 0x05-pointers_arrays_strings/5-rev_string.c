89ec505d95686fd887ec83d09c9c0f7c846c8cb4
GiovanniAyala <op7puro@live.com>
2019-10-01 21:07:51 +0000

    Fix task 05

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * print_rev - Print String reverse
+ * @s: Parameter String
+ *
+ *
+ *
+*/
+void rev_string(char *s)
+{
+int l= 0, i;
+char *end , *str ,c , *p ;
+end = s;
+str = s;
+p = s;
+while(*p)
+{
+l++;
+p++;
+}
+for (i = 0; i < l - 1; i++)
+end++;
+
+for (i = 0; i < l / 2; i++)
+{
+c = *end;
+*end = *str;
+*str = c;
+
+str++;
+end--;
+}
+}
