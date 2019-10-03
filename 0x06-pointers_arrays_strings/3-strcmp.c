81d7a2ee2ef9ccc7516c1a857b8e2a6e8aeded54
GiovanniAyala <op7puro@live.com>
2019-10-03 20:12:01 +0000

    Add task 03

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strcmp - Compare string
+ * @s1: String 1
+ * @s2: String 2
+ * Return: Return value
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+int band = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+	i++;
+	band = 1;
+}
+if (band != 0)
+{
+	i = 0;
+	band = s1[i] - s2[i];
+}
+
+return (band);
+}
