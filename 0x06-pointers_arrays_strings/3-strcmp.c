369344852de020c3dc9d7be27a37633e2fb38362
GiovanniAyala <op7puro@live.com>
2019-10-03 20:42:43 +0000

    Fix task 03

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,20 +7,24 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-int i = 0;
-int band = 0;
-while (s1[i] != '\0' && s2[i] != '\0')
+int i = 0, band;
+int n1 = 0 , n2 = 0;
+while (s1[n1] != '\0')
 {
-	if (s1[i] == s1[i])
-	band = 1;
-	else
-	band = 0;
-	i++;
+n1++;
 }
-if (band != 0)
+while (s2[n2] != '\0')
+{
+n2++;
+}
+if (n1 != n2)
 {
 	i = 0;
 	band = s1[i] - s2[i];
+}
+else
+{
+	band = 0;
 }
 
 return (band);
