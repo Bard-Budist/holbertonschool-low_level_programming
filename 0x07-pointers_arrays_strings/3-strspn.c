2527eab852ce6ec261f9e28b6160a9872a984c2c
GiovanniAyala <op7puro@live.com>
2019-10-07 20:17:50 +0000

    Add task 03

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j, n = 0;
+for (i = 0; s[i] != ' '; i++)
+{
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+	if (s[i] == accept[j])
+		n++;
+	}
+}
+return (n);
+}
