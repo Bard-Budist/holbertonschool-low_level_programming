e45c3c6b82deaa3c19f32e01c2a270851cce5708
GiovanniAyala <op7puro@live.com>
2019-10-07 17:16:35 +0000

    Fix task 02

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,27 +1,27 @@
 #include "holberton.h"
 /**
+ * _strchr - Strchar
+ * @s: String
+ * @c: Char
  *
- *
- *
- *
- *
+ * Return: Char
  *
 */
 char *_strchr(char *s, char c)
 {
 int i = 0;
 char *t;
-while(s[i] != '\0')
+while (s[i] != '\0')
 {
 if (s[i] == c)
 {
-s+=i;
+s += i;
 t = s;
 break;
 }
 else
-t = ((void*)0);
+t = ((void *)0);
 i++;
 }
-return t;
+return (t);
 }
