4deac567566892e36ea0fa5839a7eb5198572d29
GiovanniAyala <op7puro@live.com>
2019-10-07 21:54:00 +0000

    Add task 04

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strpbrk - Break
+ * @s: String
+ * @accept: Accept
+ *
+ * Return: char
+ *
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+		if (s[i] == accept[j])
+			return (s + i);
+	}
+
+}
+return ('\0');
+}
