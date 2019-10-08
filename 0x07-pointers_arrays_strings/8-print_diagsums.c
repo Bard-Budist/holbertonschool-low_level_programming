3bebf7ba09679f5fceb0704a5da7ac5e78a1a436
GiovanniAyala <op7puro@live.com>
2019-10-08 03:06:44 +0000

    Add task 08

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+void print_diagsums(int *a, int size)
+{
+int i, j, d1 = 0, d2 = 0;
+for (i = 0; i < size; i++)
+d1 += a[(size + 1) * i];
+for (j = 0; j < size; j++)
+d2 += a[(size -1) * (j + 1)];
+printf("%d, %d\n", d1, d2);
+
+
+}
