384ced1f0fab92a797f48900e21957683613da51
System Administrator <root@Holbertons-iMac-8.local>
2019-10-15 11:19:12 -0500

    Add task 02

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * str_concat - Concat string
+ * @s1: String number 1
+ * @s2: String number 2
+ * Return: Char
+ *
+ *
+*/
+char *str_concat(char *s1, char *s2)
+{
+unsigned int t1 = 0, t2 = 0, i = 0;
+if (s1 == 0 || s2 == 0)
+return ("");
+else
+{
+while (s1[t1])
+t1++;
+while (s2[t2])
+t2++;
+char *p = malloc((t1 + t2) *(sizeof(char)));
+while (i < t1)
+{
+p[i] = *s1;
+i++;
+s1++;
+}
+i = 0;
+while (i < t2)
+{
+p[i + t1] = *s2;
+i++;
+s2++;
+}
+return (p);
+}
+return (NULL);
+}
