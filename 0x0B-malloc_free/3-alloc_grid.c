c4e772f96a7bb61e2a0db2a3d05f95bd172f4b7d
System Administrator <root@Holbertons-iMac-8.local>
2019-10-15 15:29:11 -0500

    Add task 03

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - ...
+ * @width: .
+ * @height: j
+ * Return: value
+ */
+int **alloc_grid(int width, int height)
+{
+if (width < 0 || height < 0)
+return (NULL);
+int i, j;
+int **arr = malloc(width + 1  * (sizeof(int *)));
+for (i = 0; i < width; i++)
+arr[i] = malloc(height * (sizeof(int)));
+if (arr == NULL)
+return (NULL);
+for (i = 0; i < width; i++)
+{
+for (j = 0; j < height; j++)
+{
+arr[i][j] = 0;
+}
+}
+return (arr);
+}
