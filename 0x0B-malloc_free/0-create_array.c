c36240a70daf0f7c43a9c4f3a36dc853dfeda571
System Administrator <root@Holbertons-iMac-8.local>
2019-10-15 13:09:37 -0500

    Fix task 00

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,17 +8,18 @@
 */
 char *create_array(unsigned int size, char c)
 {
+char *p;
 if (size == 0)
 return (NULL);
 else
 {
 
-char *p = malloc(sizeof(char) * size);
+p = malloc(sizeof(char) * size);
 while (size--)
 {
 p[size] = c;
 }
 return (p);
 }
-return (NULL);
+return (p);
 }
