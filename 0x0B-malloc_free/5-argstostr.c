a61772690893c7e44124c7a7f96f06c02e0dccdd
GiovanniAyala <op7puro@live.com>
2019-10-16 05:15:09 +0000

    Add yask 05

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * argstostr - punt
+  * @ac: intero
+  * @av: char
+  * Return: p
+  */
+char *argstostr(int ac, char **av)
+{
+int a, b, c = 0, d = 0;
+char *p;
+if (ac == 0)
+	return (NULL);
+if (av == NULL)
+	return (NULL);
+for (a = 0; a < ac; a++)
+{
+	for (b = 0; av[a][b] != '\0'; b++)
+	{
+		c++;
+	}
+	c++;
+}
+c++;
+p = malloc(sizeof(char) * c);
+if (p == NULL)
+	return (NULL);
+for (a = 0; a < ac; a++)
+{
+	for (b = 0; av[a][b] != '\0'; b++)
+	{
+		p[d++] = av[a][b];
+	}
+	p[d++] = '\n';
+}
+p[d] = '\0';
+return (p);
+}
