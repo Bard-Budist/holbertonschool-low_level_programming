524a8a1eef20a6b75667a65fadfd8e2a646e6774
GiovanniAyala <op7puro@live.com>
2019-10-17 14:17:01 -0500

    Add task 01

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* string_nconcat - adwad
+* @s1: ..
+* @s2: ..
+* @n: ..
+* Return: ..
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, j, i1 = 0, i2 = 0;
+	char *c;
+
+	if (s1 == 0 || s2 == 0)
+	return (NULL);
+	while (s1[i1] != '\0')
+	{
+		i1++;
+	}
+	while (s2[i2] != '\0')
+	{
+		i2++;
+	}
+	i2 = (i2 > n) ? n : i2;
+	j = i1 + i2;
+	c = malloc(j);
+	if (c == NULL)
+	return (NULL);
+	for (i = 0; i < i1; i++)
+	{
+		c[i] = s1[i];
+	}
+	for (i = 0; i < i2; i++)
+	{
+		c[i + i1] = s2[i];
+	}
+	c[i + i1] = '\0';
+	return (c);
+}
