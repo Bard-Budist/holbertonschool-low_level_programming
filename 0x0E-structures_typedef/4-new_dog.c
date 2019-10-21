901d3f322c9948e833437902938392bd6bcc3d5c
GiovanniAyala <op7puro@live.com>
2019-10-21 15:30:09 -0500

    Fix task 04 v1.5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,27 +11,48 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i;
+	int i, cn = 0, co = 0;
+	char *nameC;
+	char *ownerC;
 
 	if (name == NULL || owner == NULL)
 	{
 	return (NULL);
 	}
+	while (name[cn] != '\0')
+	cn++;
+
+	while (owner[co] != '\0')
+	co++;
+	nameC = malloc(nc * sizeof(char));
+	if (nameC == NULL)
+	{
+		free(nameC);
+		return (NULL);
+	}
+	ownerC = malloc(co * sizeof(char));
+	if (ownerC == NULL)
+	{
+		free(ownerC);
+		return (NULL);
+	}
 	struct dog *doge = malloc(sizeof(struct dog));
 	if (doge == NULL)
-	return (NULL);
-	char nameC[100];
-	char ownerC[100];
+	{
+		free(doge);
+		return (NULL);
+	}
+
 	for (i = 0; name[i] != '\0'; i++)
 	{
 	nameC[i] = name[i];
-	doge->name[i] = name[i];
 	}
 	for (i = 0; owner[i] != '\0'; i++)
 	{
 		ownerC[i] = owner[i];
-		doge->owner[i] = owner[i];
 	}
+	doge->name = nameC;
+	doge->owner = ownerC;
 	doge->age = age;
 
 	return (doge);
