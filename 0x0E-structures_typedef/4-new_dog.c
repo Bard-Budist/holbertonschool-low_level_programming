82ddeecbada28d59c9c479fe09c34f375ff76801
GiovanniAyala <op7puro@live.com>
2019-10-21 14:58:52 -0500

    Fix task 04 v1.4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,16 +11,28 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *doge = malloc(sizeof(struct dog));
+	int i;
 
+	if (name == NULL || owner == NULL)
+	{
+	return (NULL);
+	}
+	struct dog *doge = malloc(sizeof(struct dog));
 	if (doge == NULL)
 	return (NULL);
-	char *nameC = name;
-	char *ownerC = owner;
-
-	doge->name = nameC;
+	char nameC[100];
+	char ownerC[100];
+	for (i = 0; name[i] != '\0'; i++)
+	{
+	nameC[i] = name[i];
+	doge->name[i] = name[i];
+	}
+	for (i = 0; owner[i] != '\0'; i++)
+	{
+		ownerC[i] = owner[i];
+		doge->owner[i] = owner[i];
+	}
 	doge->age = age;
-	doge->owner = ownerC;
 
 	return (doge);
 }
