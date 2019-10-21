cccf7ff5f91c4f77b829d023b4ec46ddc34e8897
GiovanniAyala <op7puro@live.com>
2019-10-21 12:11:33 -0500

    Add validation null

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
 * init_dog - init_dog
 * @d: Doge
@@ -9,7 +10,10 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d != NULL)
+	{
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
+	}
 }
