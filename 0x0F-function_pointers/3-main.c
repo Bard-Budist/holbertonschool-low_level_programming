14573c2e25a584f50c80a7918ce06fc52aebb776
GiovanniAyala <op7puro@live.com>
2019-10-23 16:40:53 -0500

    Fix task 02

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,25 +10,21 @@
 int main(int argc, char **argv)
 {
 	int  num1, num2;
-	char *c;
-
-	c = argv[2];
 	int (*funcion)(int, int);
 
-	funcion = get_op_func(c);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	funcion = get_op_func(argv[2]);
 	if (funcion == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 
 	int resul = funcion(num1, num2);
 
