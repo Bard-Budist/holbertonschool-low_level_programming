35d060a68fd246a0be7baedae7b56ec89644615f
GiovanniAyala <op7puro@live.com>
2019-10-26 12:05:57 -0500

    Fix task 00

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,10 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int suma = 0, i;
-
+	va_list list;
+ 	va_start(list, n);
 	if (n == 0)
 	return (0);
-	va_list list;
-
-	va_start(list, n);
-
 	for (i = 0; i < n; i++)
 	{
 		suma += va_arg(list, int);
