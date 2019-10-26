fb1d0bc5082d5a33a10bced18798e45f1f8cf0bb
GiovanniAyala <op7puro@live.com>
2019-10-26 13:23:36 -0500

    Fix task ASM

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,17 @@
-section .data
-	msg db 'Hello, Holberton', 10
+SECTION .DATA
+	hello:     db 'Hello, Holberton',10
+	helloLen:  equ $-hello
 
-section .text
-	global _main
+SECTION .TEXT
+	GLOBAL _start
 
-_main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, 17
-	syscall
+_start:
+	mov rax,4
+	mov rbx,1
+	mov rcx,hello
+	mov rdx,helloLen
+	int 80h
 
-	mov rax, 60
-    mov rdi, 0
-    syscall
+	mov rax,1
+	mov rbx,0
+	int 80h
