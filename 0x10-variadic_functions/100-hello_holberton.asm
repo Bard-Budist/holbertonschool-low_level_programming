b7e95fcbea4a139af21bc379ddf2cd7415831dc1
GiovanniAyala <op7puro@live.com>
2019-10-29 19:01:22 -0500

    Add fix

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,17 +1,17 @@
 section .data
-	str: db 'Hello, Holberton', 10,0
+	str: db "Hello, Holberton", 10,0
 
 section .text
 	global main
 
 main:
 	mov rax, 1
-	mov rbx, 1
-	mov rcx, str
+	mov rdi, 1
+	mov rsi, str
 	mov rdx, 17
 	syscall
 
 	mov rax, 60
-	mov rdx, 0
+	mov rdi, 0
 	syscall
 
