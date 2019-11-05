2fd4fda9830eee3ac4e79ce3a5b37bd018434196
GiovanniAyala <op7puro@live.com>
2019-11-05 09:43:58 -0500

    Fix task add return NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,16 +16,29 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
-	while (var < idx)
+	if (idx == 0)
 	{
-		if (aux == NULL)
-			return (NULL);
-		aux = aux->next;
-		var++;
+		next = aux;
+		new->n = n;
+		new->next = next;
+		aux->next = new;
+		return (new);
 	}
-	new->n = n;
-	next = aux->next;
-	new->next = next;
-	aux->next = new;
-	return (new);
+	else
+	{
+		while (var < idx)
+		{
+			if (aux == NULL)
+				return (NULL);
+			aux = aux->next;
+			var++;
+		}
+		next = aux->next;
+		new->n = n;
+		new->next = next;
+		aux->next = new;
+		return (new);
+	}
+	free(new);
+	return (NULL);
 }
