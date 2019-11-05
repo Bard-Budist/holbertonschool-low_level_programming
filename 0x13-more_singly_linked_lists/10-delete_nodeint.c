6cb545200a7647ad1a0d5169607d88aa2ed4dd1e
GiovanniAyala <op7puro@live.com>
2019-11-05 13:34:10 -0500

    Fix task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,27 +12,27 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *new = NULL, *aux = *head;
 	unsigned int var = 0;
 
-	while (var < index)
-	{
-		if (aux != NULL)
-		{
-			aux = aux->next;
-			var++;
-		}
-		else
-		{
-			free(new);
-			return (-1);
-		}
-	}
-
-	new = aux;
-	aux = aux->next;
-	if (aux == NULL)
-	{
+	if (head == NULL && *head == NULL)
 		return (-1);
+
+	while (aux)
+	{
+		if (index == 0)
+		{
+			new = *head;
+			*head = new->next;
+			free(new);
+			return (1);
+		}
+		if (var == index - 1)
+		{
+			new = aux->next;
+			aux->next = new->next;
+			free(new);
+			return (1);
+		}
+		var++;
+		aux =  aux->next;
 	}
-	new->next = aux->next;
-	free(aux);
-	return (1);
+	return (-1);
 }
