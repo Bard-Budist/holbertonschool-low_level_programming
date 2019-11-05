14e0277f43134ec787fc67317494594950d00019
GiovanniAyala <op7puro@live.com>
2019-11-05 08:42:12 -0500

    Fix new line and add if contition

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	while (*head != NULL)
+
+	if (head != NULL)
 	{
-		temp = *head;
-		*head = temp->next;
-		free(temp);
+		while (*head != NULL)
+		{
+			temp = *head;
+			*head = temp->next;
+			free(temp);
+		}
 	}
 	free(*head);
+
 }
