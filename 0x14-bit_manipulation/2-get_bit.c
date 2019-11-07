c6487b4d59a22aa72981958ed34f0fa3ef16d660
GiovanniAyala <op7puro@live.com>
2019-11-07 15:51:28 -0500

    Add task 02 and update header

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * get_bit - get bit
+ * @n: number
+ * @index: Index
+ * Return: Number of byte
+ *
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int cont = 0;
+
+	while (n)
+	{
+		if (index == cont)
+		{
+			return (n % 2);
+		}
+		n /= 2;
+		cont++;
+	}
+	return (-1);
+}
+
