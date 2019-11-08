42e8d2fb35f4e54b7676490cb95251f5b2ce270b
GiovanniAyala <op7puro@live.com>
2019-11-07 19:46:23 -0500

    Fix task 04

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,11 +5,10 @@
  * @index: Index
  * Return: 1 :) or -1 :(
  *
- *
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (n != NULL)
+	if (n != NULL && index < 64)
 	{
 		*n = (~(1 << index) & *n);
 		return (1);
