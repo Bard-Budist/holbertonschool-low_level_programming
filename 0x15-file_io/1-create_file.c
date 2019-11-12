24b1d2140381f0b5558e14738908ecce63619d9c
GiovanniAyala <op7puro@live.com>
2019-11-12 15:04:45 -0500

    Fix task 01

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,16 +13,12 @@ int create_file(const char *filename, char *text_content)
 	state = open(filename, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
 	if (state == -1)
 		return (-1);
-	while (text_content[count])
-		count++;
 	if (text_content != NULL)
-		write_ = write(state, text_content, count);
-	else
 	{
-		close(state);
-		return (1);
+		while (text_content[count])
+			count++;
+		write_ = write(state, text_content, count);
 	}
-
 	if (write_ == -1)
 		return (-1);
 	close(state);
