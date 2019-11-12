b2ba1988961ec3f2af4cff35053cebf811b20181
GiovanniAyala <op7puro@live.com>
2019-11-12 14:39:38 -0500

    Add task 01

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * create_file - Create file
+ * @filename: File name
+ * @text_content: Test contecnt
+ * Return: 1 if :) or -1 if :(
+ *
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int state, count = 0, write_ = 0;
+
+	state = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	if (state == -1)
+		return (-1);
+	while (text_content[count])
+		count++;
+	if (text_content != NULL)
+		write_ = write(state, text_content, count);
+
+	if (write_ == -1)
+		return (-1);
+	close(state);
+	return (1);
+}
