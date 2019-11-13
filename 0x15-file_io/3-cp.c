800f5456af05df4e6e75d722ddfb4ccdeffc257d
GiovanniAyala <op7puro@live.com>
2019-11-12 19:06:34 -0500

    Fix task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,12 +24,7 @@ int main(int argc, char const *argv[])
 		dprintf(STDERR_FILENO, "Usage: Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	stateTo = open(argv[2], O_CREAT | O_RDWR | O_APPEND | O_TRUNC, 0664);
-	if (stateTo == -1)
-	{
-		dprintf(STDERR_FILENO, "Usage: Error: Can't read from file %s\n", argv[2]);
-		exit(98);
-	}
+	stateTo = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	while ((count = read(stateFrom, buffer, 1024)) != 0)
 	{
 		count = write(stateTo, buffer, count);
@@ -42,13 +37,13 @@ int main(int argc, char const *argv[])
 	closeFrom = close(stateFrom);
 	if (closeFrom == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", closeFrom);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", closeFrom);
 		exit(100);
 	}
 	closeTo = close(stateTo);
 	if (closeTo == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", closeTo);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", closeTo);
 		exit(100);
 	}
 	return (0);
