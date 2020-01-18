b7a1a06b7e3f70bc5521d38a8cb0b3f83c85b2a2
GiovanniAyala <op7puro@live.com>
2020-01-18 17:23:23 +0000

    Fix task 00

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,26 +2,26 @@
 /**
  * hash_table_create - Create hash table
  * @size: Size
- * return: Null or hash table
+ * Return: Null or hash table
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *h1;
+	hash_table_t *h1;
 
-        if (size <= 0)
-        {
-                return (NULL);
-        }
-        h1 = malloc(sizeof(hash_table_t));
-        if (h1 == NULL)
-        {
-                return (NULL);
-        }
-        h1->array = calloc(size, sizeof(char *));
-        if (h1->array == NULL)
-        {
-                return (NULL);
-        }
-        h1->size = size;
-        return (h1);
+	if (size <= 0)
+	{
+		return (NULL);
+	}
+	h1 = malloc(sizeof(hash_table_t));
+	if (h1 == NULL)
+	{
+		return (NULL);
+	}
+	h1->array = calloc(size, sizeof(char *));
+	if (h1->array == NULL)
+	{
+		return (NULL);
+	}
+	h1->size = size;
+	return (h1);
 }
