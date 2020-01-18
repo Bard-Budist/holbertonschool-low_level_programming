227f1a61559a72e9811591804b1e799f83e5721b
GiovanniAyala <op7puro@live.com>
2020-01-18 20:51:33 +0000

    Try fix task 03

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,13 +35,8 @@ hash_node_t *add_node(hash_node_t **head, const char *str, const char *value)
 	{
 		return (NULL);
 	}
-	if (str == '\0')
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->value = _strdup((char *)value);
-	new_node->key = _strdup((char *)str);
+	new_node->value = strdup(value);
+	new_node->key = strdup(str);
 	if (*head != NULL)
 	{
 		new_node->next = *head;
@@ -55,33 +50,4 @@ hash_node_t *add_node(hash_node_t **head, const char *str, const char *value)
 		return (*head);
 	}
 	return (NULL);
-}
-
-/**
- * _strdup - str
- * @str: String paramter
- * Return: Char
- *
- *
- *
-*/
-char *_strdup(char *str)
-{
-	char *p;
-	unsigned int a = 0, i = 0;
-
-	if (str == 0)
-		return (NULL);
-	while (str[a] != '\0')
-		a++;
-	a++;
-	p = malloc(a * sizeof(char));
-	if (p == 0)
-		return (NULL);
-	while (i < a)
-	{
-		p[i] = str[i];
-		i++;
-	}
-	return (p);
 }
