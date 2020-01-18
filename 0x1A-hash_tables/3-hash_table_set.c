2a7815aa32dae474b8e573c6a181af275c17f858
GiovanniAyala <op7puro@live.com>
2020-01-18 19:16:02 +0000

    Fix task 03

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,15 +9,20 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int index = 0;
+	hash_node_t *new_node;
 
-	if (ht == NULL)
-	{
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL || ht == NULL)
 		return (0);
-	}
+
+	new_node->value = _strdup((char *) value);
+	new_node->key = _strdup((char *) key);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array == NULL)
 		return (0);
-	ht->array[index] = add_node(&ht->array[index], key, value);
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
 }
 
