b39f55b35b81899cdb9ebd9be1d806c11a1eb063
GiovanniAyala <op7puro@live.com>
2020-01-18 17:24:40 +0000

    Fix heaer

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,8 @@
 #ifndef header_file
 #define header_file
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -11,9 +13,9 @@
  **/
 typedef struct hash_node_s
 {
-        char *key;
-        char *value;
-        struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -26,10 +28,12 @@ typedef struct hash_node_s
  **/
 typedef struct hash_table_s
 {
-        unsigned long int size;
-        hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+hash_node_t *add_node(hash_node_t **head, const char *str, const char *value);
 #endif
