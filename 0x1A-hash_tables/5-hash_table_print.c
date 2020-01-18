0d8331af463088bf59154b9c0ab4b2d0ddde0f9b
GiovanniAyala <op7puro@live.com>
2020-01-18 21:38:21 +0000

    Add task 05

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - ...
+ * @ht: ...
+ *
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i = 0, band = 0;
+	hash_node_t *temp;
+
+	if (ht != NULL)
+	{
+		printf("{");
+		while (i < ht->size)
+		{
+			temp = ht->array[i];
+			while (temp != NULL)
+			{
+				if (i > 0 && band != 0)
+					printf(", ");
+				printf("'%s': '%s'", temp->key, temp->value);
+				temp = temp->next;
+				if (temp != NULL)
+					printf(", ");
+				band = 1;
+			}
+			i++;
+		}
+		printf("}\n");
+	}
+}
