893f9b944be595131be55909cf83fc10a63b810e
GiovanniAyala <op7puro@live.com>
2020-01-18 21:42:22 +0000

    Fix task 05

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,7 +22,11 @@ void hash_table_print(const hash_table_t *ht)
 				printf("'%s': '%s'", temp->key, temp->value);
 				temp = temp->next;
 				if (temp != NULL)
+				{
+					band = 0;
 					printf(", ");
+					continue;
+				}
 				band = 1;
 			}
 			i++;
