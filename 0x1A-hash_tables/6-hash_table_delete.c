908e7eaee4eb9eeea78f9b352567ea19cc32c79c
GiovanniAyala <op7puro@live.com>
2020-01-18 23:45:25 +0000

    Fix task

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,34 +1,31 @@
 #include "hash_tables.h"
 /**
- *
- *
- *
- *
+ * hash_table_delete - ...
+ * @ht: ..
  *
 */
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned int i = 0;
 	hash_node_t *temp;
+	hash_node_t *node_t;
+
 	if (ht != NULL)
 	{
 		while (i < ht->size)
 		{
-			if (ht->array[i] != NULL)
+			temp = ht->array[i];
+			while (temp != NULL)
 			{
-				temp = ht->array[i];
-				while (temp != NULL)
-				{
-					free(temp);
-					free(temp->key);
-					free(temp->value);
-					temp = temp->next;
-				}
+				node_t = temp;
+				temp = temp->next;
+				free(node_t->key);
+				free(node_t->value);
+				free(node_t);
 			}
 			i++;
 		}
 	}
-	free(temp);
 	free(ht->array);
 	free(ht);
 }
