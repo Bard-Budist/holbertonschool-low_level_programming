1ff5c4d864f0e7c13aa6e672da1bb7f8b396a2ea
GiovanniAyala <op7puro@live.com>
2020-01-18 17:44:22 +0000

    Fix task 03

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,3 +20,70 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht->array[index] = add_node(&ht->array[index], key, value);
 	return (1);
 }
+
+/**
+ * add_node - ...
+ * @head: ..
+ * @str: ..
+ * @value: ..
+ * Return: ..
+*/
+hash_node_t *add_node(hash_node_t **head, const char *str, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (str == '\0')
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = _strdup((char *)value);
+	new_node->key = _strdup((char *)str);
+	if (*head != NULL)
+	{
+		new_node->next = *head;
+		(*head) = new_node;
+		return (new_node);
+	}
+	else
+	{
+		new_node->next = NULL;
+		(*head) = new_node;
+		return (*head);
+	}
+	return (NULL);
+}
+
+/**
+ * _strdup - str
+ * @str: String paramter
+ * Return: Char
+ *
+ *
+ *
+*/
+char *_strdup(char *str)
+{
+	char *p;
+	unsigned int a = 0, i = 0;
+
+	if (str == 0)
+		return (NULL);
+	while (str[a] != '\0')
+		a++;
+	a++;
+	p = malloc(a * sizeof(char));
+	if (p == 0)
+		return (NULL);
+	while (i < a)
+	{
+		p[i] = str[i];
+		i++;
+	}
+	return (p);
+}
