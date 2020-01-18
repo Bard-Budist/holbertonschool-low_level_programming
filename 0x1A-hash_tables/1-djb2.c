7f1b14751893aa9a4b436f00b87913a4edbff195
GiovanniAyala <op7puro@live.com>
2020-01-18 17:26:05 +0000

    Add task 01

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,21 +2,18 @@
 /**
  * hash_djb2 - Return key
  * @str: String
- * return: u...
+ * Return: u...
  *
 */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-        unsigned long int hash;
-        int c;
+	unsigned long int hash;
+	int c;
 
-        if (str == NULL)
-                return (NULL);
-
-        hash = 5381;
-        while ((c = *str++))
-        {
-                hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-        }
-        return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
