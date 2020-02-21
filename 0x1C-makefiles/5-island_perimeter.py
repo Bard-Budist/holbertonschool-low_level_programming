e072f18f473d5a56724d02a5fbee3f73300cb9e5
Bard-Budist <op7puro@live.com>
2020-02-21 20:55:52 +0000

    Fix: Try check task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,6 +11,16 @@ def island_perimeter(grid):
     count = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
+            if (grid[i][j] == 0):
+                try:
+                    if (grid[i][j + 1] == 1 and grid[i][j - 1] == 1
+                            and grid[i + 1][j] == 1 and grid[i - 1][j] == 1):
+                        grid[i][j] = 2
+                except IndexError:
+                    pass
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if (grid[i][j] == 1):
                 try:
                     if (grid[i][j + 1] == 0 or grid[i][j + 1] is None):
