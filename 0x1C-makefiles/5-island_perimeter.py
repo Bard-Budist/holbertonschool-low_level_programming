e6906867d743e65949ed75d88186378abb8064a7
Bard-Budist <op7puro@live.com>
2020-02-21 19:34:47 +0000

    Fix: Fix pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,12 +12,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if (grid[i][j] == 1):
-                if (grid[i][j + 1] == 0 or grid[i][j + 1] == None):
+                if (grid[i][j + 1] == 0 or grid[i][j + 1] is None):
                     count = count + 1
-                if (grid[i][j - 1] == 0 or grid[i][j - 1] == None):
+                if (grid[i][j - 1] == 0 or grid[i][j - 1] is None):
                     count = count + 1
-                if (grid[i + 1][j] == 0 or grid[i + 1][j] == None):
+                if (grid[i + 1][j] == 0 or grid[i + 1][j] is None):
                     count = count + 1
-                if (grid[i - 1][j] == 0 or grid[i - 1][j] == None):
+                if (grid[i - 1][j] == 0 or grid[i - 1][j] is None):
                     count = count + 1
     return count
