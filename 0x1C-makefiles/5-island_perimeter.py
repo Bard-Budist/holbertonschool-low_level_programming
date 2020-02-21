8e8184e5dc48ddbd9ad61064b291caea0aebbf83
Bard-Budist <op7puro@live.com>
2020-02-21 19:33:30 +0000

    Fix: Add other condition

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,16 +5,19 @@
 
 
 def island_perimeter(grid):
+    """
+        Function found Island Perimeter
+    """
     count = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if (grid[i][j] == 1):
-                if (grid[i][j + 1] == 0):
+                if (grid[i][j + 1] == 0 or grid[i][j + 1] == None):
                     count = count + 1
-                if (grid[i][j - 1] == 0):
+                if (grid[i][j - 1] == 0 or grid[i][j - 1] == None):
                     count = count + 1
-                if (grid[i + 1][j] == 0):
+                if (grid[i + 1][j] == 0 or grid[i + 1][j] == None):
                     count = count + 1
-                if (grid[i - 1][j] == 0):
+                if (grid[i - 1][j] == 0 or grid[i - 1][j] == None):
                     count = count + 1
     return count
