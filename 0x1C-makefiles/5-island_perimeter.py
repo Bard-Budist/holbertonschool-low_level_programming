7e31dc72ee80084cbf7487a577f2809b58e61ec0
Bard-Budist <op7puro@live.com>
2020-02-21 23:38:10 +0000

    Fix: Add condition

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,27 +9,28 @@ def island_perimeter(grid):
         Function found Island Perimeter
     """
     count = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if (grid[i][j] == 1):
-                try:
-                    if (grid[i][j + 1] == 0):
-                        count = count + 1
-                except IndexError:
-                    count = count + 1
-                try:
-                    if (grid[i][j - 1] == 0):
-                        count = count + 1
-                except IndexError:
-                    count = count + 1
-                try:
-                    if (grid[i + 1][j] == 0):
-                        count = count + 1
-                except IndexError:
-                    count = count + 1
-                try:
-                    if (grid[i - 1][j] == 0):
-                        count = count + 1
-                except IndexError:
-                    count = count + 1
+    if (len(grid) <= 100 and len(grid[0]) <= 100):
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if (grid[i][j] == 1):
+                    try:
+                        if (grid[i][j + 1] == 0):
+                            count += 1
+                    except IndexError:
+                        count += 1
+                    try:
+                        if (grid[i][j - 1] == 0):
+                            count += 1
+                    except IndexError:
+                        count += 1
+                    try:
+                        if (grid[i + 1][j] == 0):
+                            count += 1
+                    except IndexError:
+                        count += 1
+                    try:
+                        if (grid[i - 1][j] == 0):
+                            count += 1
+                    except IndexError:
+                        count += 1
     return count
