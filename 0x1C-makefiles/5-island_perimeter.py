1010cbd148a35d6d6feadcc9d82fb1c5b49cdd88
Bard-Budist <op7puro@live.com>
2020-02-21 18:59:14 +0000

    Feature: Add task 05

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Perimeter if island
+"""
+
+
+def island_perimeter(grid):
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (grid[i][j + 1] == 0):
+                    count = count + 1
+                if (grid[i][j - 1] == 0):
+                    count = count + 1
+                if (grid[i + 1][j] == 0):
+                    count = count + 1
+                if (grid[i - 1][j] == 0):
+                    count = count + 1
+    return count
